046cafa941cb4105fa7c31428d81b11e38bb6cda
Ezequiel Richter <ezequielrichter57@gmail.com>
2024-01-30 19:26:02 -0300

    Aula 30/01/2024

---
+++ CWIstart-JavaScript/Aula01/main.js
@@ -0,0 +1,15 @@
+const firstName = "Ezequiel"
+const lastName = "Richter"
+
+const names = "Felipe,João,Julia"
+
+console.log('Meu nome é ' + firstName + ' ' + lastName);
+console.log(`Meu nome é ${firstName.toUpperCase()} ${lastName.toLowerCase()}`);
+
+console.log(names.split(","));
+
+const number = 5
+
+let a = [1, 1, 2]
+
+a.le
