689f92a2b9f48e0d5d90256d5e1e1027803cbc7a
Ezequiel Richter <ezequielrichter57@gmail.com>
2024-02-14 19:49:08 -0300

    aula 14/02/2024

--- CWIstart-JavaScript/Aula01/main.js
+++ CWIstart-JavaScript/Aula01/main.js
@@ -2,3 +2,6 @@ let nome = "João"
 let atividadeFavorita = "jogar"
 let favoriteFood = "pizza"
 let temperatura = 0.6
+let joao = [1,2,3]
+console.log(joao.length);
+console.log(atividadeFavorita.length);
